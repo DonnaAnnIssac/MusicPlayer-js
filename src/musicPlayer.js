1b45d583225d3389ca305c2db73204987bd71643
Donna Ann Issac <donna.ann1209@gmail.com>
2017-09-18 00:25:17 +0530

    First commit

--- src/musicPlayer.js
+++ src/musicPlayer.js
@@ -1,28 +1,68 @@
-var sound, playbtn, pausebtn, mutebtn, seekSlider, volumeSlider, currTime, durationTime
+var playlist = [{
+  title: 'Dance of death',
+  src: 'music/danceofdeath.mp3'
+},
+{
+  title: 'Paint it Black',
+  src: 'music/paint-it-black.mp3'
+},
+{
+  title: 'Satisfaction',
+  src: 'music/Satisfaction.mp3'
+}]
+
+var player = {
+  sound: new Howl({src: playlist[0].src}),
+  soundIndex: 0,
+  playing: false
+}
+
+playbtn = document.getElementById('playBtn')
+pausebtn = document.getElementById('pauseBtn')
+prevbtn = document.getElementById('prevBtn')
+nextbtn = document.getElementById('nextBtn')
+playbtn.addEventListener('click', () => {player.playing = true;player.sound.play()})
+pausebtn.addEventListener('click', () => {player.playing = false;player.sound.pause()})
+nextbtn.addEventListener('click', () => { index = (player.soundIndex < playlist.length - 1) ? player.soundIndex + 1 : 0
+                                          sound = new Howl ({src: playlist[index].src})
+                                          playSelected(sound, index)
+                                        })
+prevbtn.addEventListener('click', () => { index = (player.soundIndex > 0) ? player.soundIndex - 1 : playlist.length - 1
+                                          sound = new Howl ({src: playlist[index].src})
+                                          playSelected(sound, index)
+                                        })
 function initAudioPlayer() {
-    sound = new Howl({
-      src: ['music/danceofdeath.mp3'],
-      loop: true,
-      volume: 1,
-      })
-    playbtn = document.getElementById('playBtn')
-    pausebtn = document.getElementById('pauseBtn')
-    // mutebtn = document.getElementById('mutebtn')
-    // seekSlider = document.getElementById('seekSlider')
-    // volumeSlider = document.getElementById('volumeSlider')
-    // currTime = document.getElementById('currTime')
-    // durationTime = document.getElementById('durationTime')
-    playbtn.addEventListener('click', function() {sound.play()})
-    pausebtn.addEventListener('click', function() {sound.pause()})
-    // mutebtn.addEventListener('click', function() {sound.mute()})
+  var playlistContainer = document.createElement('div')
+  playlist.forEach( (song, index) => {
+    var listItem = createListItem(song,index)
+    playlistContainer.appendChild(listItem)
+  })
+  var parent = document.getElementById('container')
+  parent.appendChild(playlistContainer)
+}
+
+function createListItem(song, index) {
+  var listItem = document.createElement('div')
+  var title = document.createTextNode(song.title)
+  var sound = new Howl({
+    src: [song.src],
+    volume: 1,
+  })
+  listItem.appendChild(title)
+  listItem.addEventListener('click', () => {playSelected(sound, index)})
+  return listItem
+}
+
+function playSelected(sound, index) {
+  if(player.playing === false) {
+    player.playing = true
   }
-// function mute() {
-//     if(sound.muted) {
-//       sound.muted = false;
-//        mutebtn.background = "url(volume_off.png)"
-//     }
-//     else {
-//       sound.mute()
-//     }
-//   }
+  else {
+    player.sound.pause()
+  }
+  player.soundIndex = index
+  player.sound = sound
+  player.sound.play()
+}
+
 window.addEventListener('load', initAudioPlayer)
